f999e3802c91cf5a585d19657d3b40e93a4e14ac
AdeSanyaOlu <47145002+Adesanyaolu@users.noreply.github.com>
2021-06-26 20:07:48 +0100

    Add files via upload
    Task 1
    Write a range function that takes two arguments, start and end, and returns an array containing all the numbers from start up to (and including) end. Next, write a sum function that takes an array of numbers and returns the sum of these numbers.

---
+++ task1.php
@@ -0,0 +1,19 @@
+  <?php 
+    # Task 1a
+    echo "Task 1a Solution <br>";
+        $number = range (0, 10);
+        print_r ($number);
+    
+    # Task 1b
+    echo "<br> <br> Task 1b Solution <br>";
+        function sum ($num1, $num2) {
+           return $num1 + $num2 ."<br>";
+        }
+        
+    echo sum (56, 24);
+    echo sum (33, 244);
+?>
+    
+    
+    
+    
